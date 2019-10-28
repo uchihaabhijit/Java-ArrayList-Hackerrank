f9ee524a8cf023056bab7545793601aa501b2afa
Abhijit Borkakoty <a.borkakaty@outlook.com>
2019-10-28 13:26:22 +0530

    Update ArrayList.java

--- ArrayList.java
+++ ArrayList.java
@@ -7,7 +7,9 @@ You are given  lines. In each line there are zero or more integers. You need to 
 Take your input from System.in.
 
 Input Format
-The first line has an integer . In each of the next  lines there will be an integer  denoting number of integers on that line and then there will be space-separated integers. In the next line there will be an integer denoting number of queries. Each query will consist of two integers  and .
+The first line has an integer . In each of the next  lines there will be an integer
+denoting number of integers on that line and then there will be space-separated integers. 
+In the next line there will be an integer denoting number of queries. Each query will consist of two integers.
 
 Constraints
 
